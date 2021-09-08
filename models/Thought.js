f14fcf8f45feac75d7c9638ae5a19a9e20147f03
praiseopara <cpopara2010@yahoo.com>
2021-09-08 01:56:56 -0500

    created reaction schema and virtual for thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,25 @@
 const { Schema, model, Types } = require('mongoose');
 
-// const reactionSchema = new Schema({
-//     type: 
-// })
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // get:
+    }
+})
 
 const ThoughtSchema = new Schema({
     thoughtText: {
@@ -19,9 +36,13 @@ const ThoughtSchema = new Schema({
     username: {
         type: String,
         required: true
-    }
-    // ,
-    // reactions: [reactionSchema]
+    },
+    reactions: [reactionSchema]
+});
+
+//virtual to retrieve reactions
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 });
 
 const Thought = model('Thought', ThoughtSchema);
